462d842de915fc9206126e4e908576acdc15daf3
Bruno Fetzer <brunofcrosa@hotmail.com>
2025-05-26 15:20:07 -0300

    changes

--- portfolio/wsgi.py
+++
@@ -1,9 +0,0 @@
-import sys
-from pathlib import Path
-import os
-
-from django.core.wsgi import get_wsgi_application
-sys.path.append(str(Path(__file__).resolve().parent.parent))
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio.settings')
-
-application = get_wsgi_application()
